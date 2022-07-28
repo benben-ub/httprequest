906a805dd083d66610e86d401c30cb4fc60dd621
benben-ub <adam1001ben@gmail.com>
2022-07-28 13:12:44 +0800

    test1.4

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ app = Flask(__name__)
 # LINE 聊天機器人的基本資料
 config = configparser.ConfigParser()
 config.read('config.ini')
-line_bot_api = LineBotApi('6gOQ5E/06+dqkzAUAqoP7tvHNVHRAuaiX5ZFyA9pGOEOGPw0nyuN90ARUvZa7KLzeJJxClkL08A4cc96PLDe8PB9ENoJt+yk5YKe9eXLYBE/vtbrEOWLK4z3Z1TlZlkuEc/OLVE+kvVeeTiqCXDLSwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('678c31199ad1c9ed93f3339d8f3c72fa')
+#line_bot_api = LineBotApi('6gOQ5E/06+dqkzAUAqoP7tvHNVHRAuaiX5ZFyA9pGOEOGPw0nyuN90ARUvZa7KLzeJJxClkL08A4cc96PLDe8PB9ENoJt+yk5YKe9eXLYBE/vtbrEOWLK4z3Z1TlZlkuEc/OLVE+kvVeeTiqCXDLSwdB04t89/1O/w1cDnyilFU=')
+#handler = WebhookHandler('678c31199ad1c9ed93f3339d8f3c72fa')
 
 #   接收 LINE 的資訊
 user_id='1657339819'
-@app.route('/push_function/<string:push_message>')
+@app.route("/push_function/<string:push_message>")
 def push_message(push_message):
     line_bot_api.push_message(user_id,TextSendMessage(text=push_message))
 
