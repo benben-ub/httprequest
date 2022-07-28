ad598a2651df9ca5ce57537b83c960e149be8ab1
benben-ub <adam1001ben@gmail.com>
2022-07-28 17:02:53 +0800

    test1.7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# LINE 聊天機器人的基本資料
+
 
 
 from __future__ import unicode_literals
@@ -13,16 +13,41 @@ import configparser
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-#config = configparser.ConfigParser()
-#config.read('config.ini')
+config = configparser.ConfigParser()
+config.read('config.ini')
+line_bot_api = LineBotApi('6gOQ5E/06+dqkzAUAqoP7tvHNVHRAuaiX5ZFyA9pGOEOGPw0nyuN90ARUvZa7KLzeJJxClkL08A4cc96PLDe8PB9ENoJt+yk5YKe9eXLYBE/vtbrEOWLK4z3Z1TlZlkuEc/OLVE+kvVeeTiqCXDLSwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('678c31199ad1c9ed93f3339d8f3c72fa')
 
-#   接收 LINE 的資訊
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# 學你說話
+
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
 user_id='1657339819'
 @app.route("/push_function/<string:push_text_str>")
 def push_message(push_text_str):
     line_bot_api.push_message(user_id, TextSendMessage(text=push_text_str))
-
+#   接收 LINE 的資訊
 
 if __name__ == "__main__":
     app.run()
